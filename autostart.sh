861b869a5710cd02f895d83441237f2e11768581
Jean Chiclla <j-c_f@outlook.com>
2022-03-31 09:35:47 -0500

    primer commit - config qtile

---
+++ autostart.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# systray battery icon
+# cbatticon -u 5 &
+# systray volume
+# volumeicon &
+
+# activa tap de touchpad
+touchpad.sh
+bash config-screens.sh &
+nm-applet &
+
+# coloca el fondo
+# feh --bg-scale Downloads/armenia_syunik_tatev_hayk__annihilation-wallpaper-1366x768.jpg
+# feh --bg-scale .config/qtile/earth_4k-wallpaper-1366x768.jpg
+# feh --bg-scale ~/wallpapers/wallpaperflare.com_wallpaper.jpg
+feh --bg-scale ~/wallpapers/large/matteo-catanese-PI8Hk-3ZcCU-unsplash.jpg
+
+# iniciar picom (responsable de la transparencia)
+# picom &
+
+#kdeconnect-app &
+kdeconnect-indicator &
